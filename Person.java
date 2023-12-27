55280040b59afe975f3984b4674b1cf8497bf428
Sofya Aheyeva <zozolulu1894@gmail.com>
2023-12-27 17:49:06 +0100

    commit-4

--- Person.java
+++ Person.java
@@ -2,7 +2,9 @@ import java.math.BigDecimal;
 
 public class Person {
     private String name;
+    private String surname;
     private BigDecimal salary;
+
     public Integer age(){
         return 50;
     }
