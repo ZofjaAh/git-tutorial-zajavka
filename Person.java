e717cb1a5aa97657bf4b4f2f181f39b7daa3e357
Sofya Aheyeva <zozolulu1894@gmail.com>
2023-12-27 18:03:46 +0100

    commit-6

--- Person.java
+++ Person.java
@@ -5,6 +5,9 @@ public class Person {
     private String surname;
     private BigDecimal salary;
     private Long pesel;
+    public String whatIsYourHobby(){
+        return "Drinking";
+    }
 
     public Integer age(){
         return 50;
